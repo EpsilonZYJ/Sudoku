0ab948c726853383b9bb8442e05d8c3792a85a45
HelloWorldWin <143871992+EpsilonZYJ@users.noreply.github.com>
2024-08-28 15:32:51 +0800

    SAT_update

--- X_Sudoku/include/X_Sudoku.h
+++ X_Sudoku/include/X_Sudoku.h
@@ -2,8 +2,15 @@
 #ifndef SUDOKU_X_SUDOKU_H
 #define SUDOKU_X_SUDOKU_H
 
+#include <stdlib.h>
 #include "../../SAT/include/SAT.h"
 #include "../../OptSAT/include/OptSAT.h"
+
+#define EMPTY -1
+
+typedef struct Sudoku{
+    int table[9][9];
+}Sudoku;
 
 void test2();
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,68 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 #include "SAT/include/SAT.h"
 #include "OptSAT/include/OptSAT.h"
 #include "X_Sudoku/include/X_Sudoku.h"
+
+/*
+ * 获取cnf的文件名
+ */
+char* getFileName(char* filepath){
+    int len = strlen(filepath);
+    char* ansName = (char *) malloc(sizeof(char)*len);
+    char* p = strstr(filepath, ".cnf");
+    for(;p >= filepath && *p != '/'; p --);
+    p ++;
+
+    int i;
+    for(i = 0; *(p+i) != '.'; i ++){
+        ansName[i] = *(p + i);
+    }
+    ansName[i] = '.';
+    ansName[i+1] = 'r';
+    ansName[i+2] = 'e';
+    ansName[i+3] = 's';
+    ansName[i+4] = '\0';
+    char* tmp = (char*) malloc(sizeof(char) * (strlen("./Answer/") + len));
+    strcpy(tmp, "./Answer/");
+    strcat(tmp, ansName);
+    free(ansName);
+    ansName = tmp;
+    return ansName;
+}
+
+bool fileExist(char* path){
+    struct stat buffer;
+    return (stat(path, &buffer) == 0);
+}
+
+void writeAnswer(Answer ans, char* filepath){
+    char* st = (char*) malloc(sizeof(char) * 15);
+    strcpy(st, "./Answer");
+    if(!fileExist(st))
+        system("mkdir Answer");
+    free(st);
+    FILE* fout = fopen(filepath, "w+");
+    if(fout == NULL){
+        printf("Cannot open the file!\n");
+        return;
+    }
+    if(ans.solved == false)
+        fprintf(fout, "UNSAT\n");
+    else{
+        for(int i = 1; i <= ans.numBoolen; i ++){
+            if(ans.state[i] == POSITIVE)
+                fprintf(fout, "%d ", i);
+            else if(ans.state[i] == NEGATIVE)
+                fprintf(fout, "-%d ", i);
+            else
+                fprintf(fout, "+-%d ", i);
+        }
+        fprintf(fout, "\n");
+    }
+    fclose(fout);
+}
 
 void CNFtest(char* filepath){
     clock_t start, finish;
@@ -33,6 +94,9 @@ void CNFtest(char* filepath){
                 printf("+-%d ", i);
         }
         printf("\n");
+        char* ansName = getFileName(filepath);
+        writeAnswer(ans, ansName);
+        free(ansName);
     }
     else
         printf("UNSOLVED!\n");
