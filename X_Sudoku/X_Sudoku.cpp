d25449abc4872836e97757337fa9170f283dc2cb
HelloWorldWin <143871992+EpsilonZYJ@users.noreply.github.com>
2024-08-29 15:09:32 +0800

    update Sudoku

--- X_Sudoku/X_Sudoku.cpp
+++ X_Sudoku/X_Sudoku.cpp
@@ -8,12 +8,73 @@ Hole randomLocation(){
     return h;
 }
 
+bool rulesExist(char* filepath){
+    struct stat buffer;
+    return (stat(filepath, &buffer) == 0);
+}
+
+inline int getLiteral(int row, int col, int num){
+    return (row-1)*81 + (col-1)*9 + num;
+}
+
+void writeRules(int type){
+     char* s;
+    if(type == NORMAL){
+        s = (char*) malloc(sizeof(char)*20);
+        strcpy(s, "SudokuRules.cnf");
+    }
+    else{
+        s = (char*) malloc(sizeof(char)*30);
+        strcpy(s, "DiagonalSudokuRules.cnf");
+    }
+    if(rulesExist(s))
+        return;
+    FILE* fout = fopen(s, "w+");
+    int row, col, num;
+
+    //每个格子只能填入1~9中的一个数字
+    for(row = 1; row <=9; row ++){
+        for(col = 1; col <= 9; col ++){
+            for(num = 1; num <= 8; num ++){
+                for(int i = num + 1; i <= 9; i ++){
+                    fprintf(fout, "-%d -%d 0\n", getLiteral(row, col, num), getLiteral(row, col, i));
+                }
+            }
+        }
+    }
+
+    //每行每个数字至多出现一次
+    for(row = 1; row <= 9; row ++){
+        for(num = 1; num <= 9; num ++){
+            for(col = 1; col <= 8; col ++){
+                for(int i = col+1; i <= 9; i ++){
+                    fprintf(fout, "-%d -%d 0\n", getLiteral(row, col, num), getLiteral(row, i, num));
+                }
+            }
+        }
+    }
+
+    //每列每个数字至多出现一次
+    for(col = 1; col <= 9; col ++){
+        for(num = 1; num <= 9; num ++){
+            for(row = 1; row <= 8; row ++){
+                for(int i = row+1; i <= 0; i ++){
+                    fprintf(fout, "-%d -%d 0\n", getLiteral(row, col, num), getLiteral(i, col, num));
+                }
+            }
+        }
+    }
+
+}
+
 void initSudoku(Sudoku& s){
     for(int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++){
             s.table[i][j] = EMPTY;
         }
     }
+    s.numFilled = 0;
+    s.numHoles = 0;
 }
 
 void test2(){
--- X_Sudoku/include/X_Sudoku.h
+++ X_Sudoku/include/X_Sudoku.h
@@ -3,20 +3,31 @@
 #define SUDOKU_X_SUDOKU_H
 
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include "../../SAT/include/SAT.h"
 #include "../../OptSAT/include/OptSAT.h"
 
 #define EMPTY 0
-
-typedef struct Sudoku{
-    int table[9][9];
-}Sudoku;
+#define MAX_HOLES 64 //根据文献数独最多只能挖64个洞
+#define NORMAL 1    //普通数独
+#define DIAGONAL 2  //对角线数独
 
 typedef struct Hole{
     int x;
     int y;
 }Hole;
 
+typedef struct Sudoku{
+    int table[9][9];
+    int numHoles;
+    int numFilled;
+    Hole holes[MAX_HOLES];
+}Sudoku;
+
+bool rulesExist(char* file);
+inline int getLiteral(int row, int col, int num);
+void writeRules();
 Hole randomLocation();
 void initSudoku(Sudoku& s);
 bool las_vegas(int n);
